87be1a9653fa74b1af2ff912f670d31a03405356
Pedro Lanhoso <pelan05@gmail.com>
2018-05-07 18:13:09 +0100

    initial commit 1
    added images folder, images paths in 'newclass.java' (which is a temp name), added 4 packages according to previously seen architechture

---
+++ PA/src/iu/Texto/interfaceU.java
@@ -0,0 +1,11 @@
+package iu.Texto;
+import logicaJogo.*;
+
+
+public class interfaceU {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ PA/src/logicaJogo/NewClass.java
@@ -0,0 +1,19 @@
+package logicaJogo;
+
+
+public class NewClass {
+
+/*
+images/Back.jpg
+images/Enemy.jpg
+images/Status.jpg
+images/IMG1.jpg
+images/IMG2.jpg
+images/IMG3.jpg
+images/IMG4.jpg
+images/IMG5.jpg
+images/IMG6.jpg
+images/IMG7.jpg
+*/
+    
+}

2e71560d8c1cad64f62a28a41428fd4833d56c54
Pedro Lanhoso <pelan05@gmail.com>
2018-05-08 16:59:29 +0100

    added path strings

--- PA/src/logicaJogo/NewClass.java
+++ PA/src/logicaJogo/NewClass.java
@@ -3,24 +3,20 @@ import java.util.*;
 
 public class NewClass {
 
-
     
+    
+String back = "images/Back.jpg";
+String enemy = "images/Enemy.jpg";
+String status = "images/Status.jpg";
+String img1 = "images/IMG1.jpg";
+String img2 = "images/IMG2.jpg";
+String img3 = "images/IMG3.jpg";
+String img4 = "images/IMG4.jpg";
+String img5 = "images/IMG5.jpg";
+String img6 = "images/IMG6.jpg";
+String img7 = "images/IMG7.jpg";
+    
+
+
+
 }
-
-
-
-
-
-
-/*
-images/Back.jpg
-images/Enemy.jpg
-images/Status.jpg
-images/IMG1.jpg
-images/IMG2.jpg
-images/IMG3.jpg
-images/IMG4.jpg
-images/IMG5.jpg
-images/IMG6.jpg
-images/IMG7.jpg
-*/

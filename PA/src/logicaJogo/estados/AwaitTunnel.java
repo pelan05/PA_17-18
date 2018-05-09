556d62909e0dba674dbdee4c414ea62db4196541
Pedro Lanhoso <pelan05@gmail.com>
2018-05-09 15:53:40 +0100

    Estados

--- PA/src/logicaJogo/estados/AwaitTunnel.java
+++ PA/src/logicaJogo/estados/AwaitTunnel.java
@@ -3,6 +3,75 @@ import java.util.*;
 import logicaJogo.*;
 
 
-public class AwaitTunnel {
+public class AwaitTunnel implements Estado{
+
+    @Override
+    public Estado drawsTopCard() {return this;}
+
+    @Override
+    public Estado endOfActions() {return this;}
+
+    @Override
+    public Estado winGame() {return this;}
+
+    @Override
+    public Estado loseGame() {
+        
+        //lose the game
+        
+        return this;
+    }
+
+    @Override
+    public Estado archersAttack() {return this;}
+
+    @Override
+    public Estado endArchersAttack() {return this;}
+
+    @Override
+    public Estado boilingWater() {return this;}
+
+    @Override
+    public Estado endBoilingWater() {return this;}
+
+    @Override
+    public Estado tunnelMovement() {return this;}
+
+    
+    
+    @Override
+    public Estado freeTunnelMovement() {
+        
+        return new AwaitPlayerAction();
+        
+    }
+
+    @Override
+    public Estado paidTunnelMovement() {
+        
+        return new AwaitPlayerAction();
+        
+    }
+
+     
+    
+    
+    @Override
+    public Estado rallyTroops() {return this;}
+
+    @Override
+    public Estado endRallyTroops() {return this;}
+
+    @Override
+    public Estado closeCombat() {return this;}
+
+    @Override
+    public Estado coupure() {return this;}
+
+    @Override
+    public Estado supplyRaid() {return this;}
+
+    @Override
+    public Estado sabotage() {return this;}
     
 }
--- PA/src/logicaJogo/estados/Estado.java
+++ PA/src/logicaJogo/estados/Estado.java
@@ -4,13 +4,40 @@ import logicaJogo.*;
 
 
 public interface Estado {
-    
-    Estado AwaitTopCard();
-    Estado AwaitPlayerAction();
-    Estado AwaitBeginning();
-    Estado AwaitEnemyTrackArchers();
-    Estado AwaitEnemyTrackBoiling();
-    Estado AwaitTunnel();
-    Estado AwaitSuppliesBoost();
+    //17 metodos
+    //1
+    public Estado drawsTopCard();
+    //2
+    public Estado endOfActions();
+    //3
+    public Estado winGame();
+    //4
+    public Estado loseGame();
+    //5
+    public Estado archersAttack();
+    //6
+    public Estado endArchersAttack();
+    //7
+    public Estado boilingWater();
+    //8
+    public Estado endBoilingWater();
+    //9
+    public Estado tunnelMovement();
+    //10
+    public Estado freeTunnelMovement();
+    //11
+    public Estado paidTunnelMovement();
+    //12
+    public Estado rallyTroops();
+    //13
+    public Estado endRallyTroops();
+    //14
+    public Estado closeCombat();
+    //15
+    public Estado coupure();
+    //16
+    public Estado supplyRaid();
+    //17
+    public Estado sabotage();
     
 }

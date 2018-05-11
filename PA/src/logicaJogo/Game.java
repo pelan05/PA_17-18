0d7eaf933f7bce77b5e824334ce20182515933d3
Pedro Lanhoso <pelan05@gmail.com>
2018-05-11 16:42:29 +0100

    game.java1

--- PA/src/logicaJogo/Game.java
+++ PA/src/logicaJogo/Game.java
@@ -8,6 +8,7 @@ import logicaJogo.estados.*;
 
 public class Game{
     protected int actionPoints;
+    protected int day;
     protected Card enemy;
     protected Card status;
     protected ArrayList <EventCard> arr = new ArrayList <EventCard>();
@@ -17,12 +18,29 @@ public class Game{
     this.enemy = new EnemyCard();
     this.status = new StatusCard();
         for (int i = 0; i < 7 ; i++) {
-            arr.add(new EventCard());
+            this.arr.add(new EventCard());
         }
-    
-        
-        
+    this.actionPoints = 0;
+    this.day = 0;
     }
+    
+    public void setActionPoints(int ap){
+        this.actionPoints = ap;
+    }
+    
+    public int getActionPoints(){
+        return this.actionPoints;
+    }
+    
+    public int getDay(){
+        return this.day;
+    }
+    
+    public void addDay(){
+        this.day++;
+    }
+    
+    
     
     
     
